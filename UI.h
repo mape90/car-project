88f88c05e9bad5240233c888551f973b32ac5ae4
otormane <olli.tormanen@aalto.fi>
2014-03-24 18:25:53 +0200

    Update UI.h

--- UI.h
+++ UI.h
@@ -2,7 +2,13 @@
 #define UI_HH
 
 void reportError(uint8_t errNo);
-void updateLcd(void);
 void handleButtonPress(void);
+
+void USART_Transmit( unsigned char data);
+unsigned char USART_Receive(void);
+void USART_LCD_Init(unsigned int ubrr);
+void LCD_state(uint8_t state);
+void LCD_loops(uint8_t loop);
+void LCD_clear(void);
 
 #endif
