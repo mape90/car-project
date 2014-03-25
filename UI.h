d9396b9c79315b323ef49ce6c39e27249eca25ac
otormane <olli.tormanen@aalto.fi>
2014-03-25 11:37:16 +0200

    Update UI.h

--- UI.h
+++ UI.h
@@ -3,20 +3,22 @@
 
 #include "includes.h"
 
+void LCD_update();
 void handleButtonPress(void);
+
+void USART_Transmit( unsigned char data);
 unsigned char USART_Receive(void);
 void USART_LCD_Init(unsigned int ubrr);
-void LCD_update();
+
+void LCD_Write_String(char* str, uint8_t row)
 void LCD_state(uint8_t state);
-void LCD_loops(uint8_t loop);
+void LCD_error(char* error);
+void LCD_speed(char* speed);
 void LCD_clear(void);
 
 void LCD_Write(void);
-
-void LCD_ClearErrors(void);
-
-void LCD_WriteError(uint8_t errNo);
-
+void LCD_ClearErrorFlags(void);
+void LCD_WriteErrorFlags(uint8_t errNo);
 void LCD_setTimer(void);
 
 
