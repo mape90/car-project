8bee01bc9309b9562de81417160b0d80e8e01f68
mape90 <markus.peltola@aalto.fi>
2014-03-26 15:31:23 +0200

    new Tests for motor

--- Tests.c
+++ Tests.c
@@ -8,20 +8,39 @@ void test_motor_loop()
 	writeMotorPWM(5000);
 }
 
+void test_tachometer_loop(){
+    writeMotorPWM(5000);
+    LCD_clear();
+    LCD_Write_int((int)readTachometer());
+}
+
+void test_tachometer_rpm_loop(){
+    writeMotorPWM(5000);
+    LCD_clear();
+    LCD_Write_int(tachometer2rpm(readTachometer()));
+}
+
+void test_tachometer_PI_loop(){
+    writeMotorPWM(5000);
+    LCD_clear();
+    LCD_Write_int(motorPI(tachometer2rpm(readTachometer()) -1000 ));
+}
+
+
 void test_controll_loop()
 { //test everything elese than states
     uint8_t angle;
     int speed;
 
     char error = calcError(readBumper());
-   
+
     //setState(error);
     calcControl(error, &speed, &angle);
     LCD_clear();
     LCD_error(error);
-	
+
 	//LCD_Write_String(error,1);
-	
+
 	//_delay_ms(500);
     executeControl(0, angle);
     gLastError = error;
