a69de9ceadaa5e393aa39d48fbfd4d700b73e2f5
Janne Suomalainen <janne.suomalainen@aalto.fi>
2014-03-26 13:45:50 +0200

    fixed some printing

--- Tests.c
+++ Tests.c
@@ -11,17 +11,17 @@ void test_motor_loop()
 void test_controll_loop()
 { //test everything elese than states
     uint8_t angle;
-    int speed;
+    char speed;
 
     char error = calcError(readBumper());
-   
+
     //setState(error);
     calcControl(error, &speed, &angle);
     LCD_clear();
     LCD_error(error);
-	
+
 	//LCD_Write_String(error,1);
-	
+
 	//_delay_ms(500);
     executeControl(0, angle);
     gLastError = error;
